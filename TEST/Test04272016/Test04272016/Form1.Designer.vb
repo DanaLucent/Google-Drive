c9ddef7bf88e87aa13a0c10263d1c5a753945db2
DanaLucent <danalucent@gmail.com>
2016-04-27 19:36:37 -0400

    Test project
    Here is a test project

---
+++ TEST/Test04272016/Test04272016/Form1.Designer.vb
@@ -0,0 +1,283 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.RidingGearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelmetsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ElectronicsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AccessoriesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MensToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WomensToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.JacketsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PantsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BootsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GlovesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeatherToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeatherToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextileToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LeatherToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextileToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MensToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WomensToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FullToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HalfToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FullToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HalfToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GPSToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RadioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LightingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SkullToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CrossToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SymobolicToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RidingGearToolStripMenuItem, Me.HelmetsToolStripMenuItem, Me.ElectronicsToolStripMenuItem, Me.AccessoriesToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(136, 92)
+        '
+        'RidingGearToolStripMenuItem
+        '
+        Me.RidingGearToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MensToolStripMenuItem, Me.WomensToolStripMenuItem})
+        Me.RidingGearToolStripMenuItem.Name = "RidingGearToolStripMenuItem"
+        Me.RidingGearToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.RidingGearToolStripMenuItem.Text = "Riding Gear"
+        '
+        'HelmetsToolStripMenuItem
+        '
+        Me.HelmetsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MensToolStripMenuItem1, Me.WomensToolStripMenuItem1})
+        Me.HelmetsToolStripMenuItem.Name = "HelmetsToolStripMenuItem"
+        Me.HelmetsToolStripMenuItem.Size = New System.Drawing.Size(135, 22)
+        Me.HelmetsToolStripMenuItem.Text = "Helmets"
+        '
+        'ElectronicsToolStripMenuItem
+        '
+        Me.ElectronicsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GPSToolStripMenuItem, Me.RadioToolStripMenuItem, Me.LightingToolStripMenuItem})
+        Me.ElectronicsToolStripMenuItem.Name = "ElectronicsToolStripMenuItem"
+        Me.ElectronicsToolStripMenuItem.Size = New System.Drawing.Size(135, 22)
+        Me.ElectronicsToolStripMenuItem.Text = "Electronics"
+        '
+        'AccessoriesToolStripMenuItem
+        '
+        Me.AccessoriesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SkullToolStripMenuItem, Me.CrossToolStripMenuItem, Me.SymobolicToolStripMenuItem})
+        Me.AccessoriesToolStripMenuItem.Name = "AccessoriesToolStripMenuItem"
+        Me.AccessoriesToolStripMenuItem.Size = New System.Drawing.Size(135, 22)
+        Me.AccessoriesToolStripMenuItem.Text = "Accessories"
+        '
+        'MensToolStripMenuItem
+        '
+        Me.MensToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.JacketsToolStripMenuItem, Me.PantsToolStripMenuItem, Me.BootsToolStripMenuItem, Me.GlovesToolStripMenuItem})
+        Me.MensToolStripMenuItem.Name = "MensToolStripMenuItem"
+        Me.MensToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.MensToolStripMenuItem.Text = "Men's"
+        '
+        'WomensToolStripMenuItem
+        '
+        Me.WomensToolStripMenuItem.Name = "WomensToolStripMenuItem"
+        Me.WomensToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.WomensToolStripMenuItem.Text = "Women's"
+        '
+        'JacketsToolStripMenuItem
+        '
+        Me.JacketsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LeatherToolStripMenuItem, Me.TextileToolStripMenuItem})
+        Me.JacketsToolStripMenuItem.Name = "JacketsToolStripMenuItem"
+        Me.JacketsToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.JacketsToolStripMenuItem.Text = "Jacket's"
+        '
+        'PantsToolStripMenuItem
+        '
+        Me.PantsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LeatherToolStripMenuItem1, Me.TextileToolStripMenuItem1})
+        Me.PantsToolStripMenuItem.Name = "PantsToolStripMenuItem"
+        Me.PantsToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PantsToolStripMenuItem.Text = "Pant's"
+        '
+        'BootsToolStripMenuItem
+        '
+        Me.BootsToolStripMenuItem.Name = "BootsToolStripMenuItem"
+        Me.BootsToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.BootsToolStripMenuItem.Text = "Boot's"
+        '
+        'GlovesToolStripMenuItem
+        '
+        Me.GlovesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LeatherToolStripMenuItem2, Me.TextileToolStripMenuItem2})
+        Me.GlovesToolStripMenuItem.Name = "GlovesToolStripMenuItem"
+        Me.GlovesToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.GlovesToolStripMenuItem.Text = "Gloves"
+        '
+        'LeatherToolStripMenuItem
+        '
+        Me.LeatherToolStripMenuItem.Name = "LeatherToolStripMenuItem"
+        Me.LeatherToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.LeatherToolStripMenuItem.Text = "Leather"
+        '
+        'TextileToolStripMenuItem
+        '
+        Me.TextileToolStripMenuItem.Name = "TextileToolStripMenuItem"
+        Me.TextileToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.TextileToolStripMenuItem.Text = "Textile"
+        '
+        'LeatherToolStripMenuItem1
+        '
+        Me.LeatherToolStripMenuItem1.Name = "LeatherToolStripMenuItem1"
+        Me.LeatherToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.LeatherToolStripMenuItem1.Text = "Leather"
+        '
+        'TextileToolStripMenuItem1
+        '
+        Me.TextileToolStripMenuItem1.Name = "TextileToolStripMenuItem1"
+        Me.TextileToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.TextileToolStripMenuItem1.Text = "Textile"
+        '
+        'LeatherToolStripMenuItem2
+        '
+        Me.LeatherToolStripMenuItem2.Name = "LeatherToolStripMenuItem2"
+        Me.LeatherToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.LeatherToolStripMenuItem2.Text = "Leather"
+        '
+        'TextileToolStripMenuItem2
+        '
+        Me.TextileToolStripMenuItem2.Name = "TextileToolStripMenuItem2"
+        Me.TextileToolStripMenuItem2.Size = New System.Drawing.Size(152, 22)
+        Me.TextileToolStripMenuItem2.Text = "Textile"
+        '
+        'MensToolStripMenuItem1
+        '
+        Me.MensToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FullToolStripMenuItem, Me.HalfToolStripMenuItem})
+        Me.MensToolStripMenuItem1.Name = "MensToolStripMenuItem1"
+        Me.MensToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.MensToolStripMenuItem1.Text = "Men's"
+        '
+        'WomensToolStripMenuItem1
+        '
+        Me.WomensToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FullToolStripMenuItem1, Me.HalfToolStripMenuItem1})
+        Me.WomensToolStripMenuItem1.Name = "WomensToolStripMenuItem1"
+        Me.WomensToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.WomensToolStripMenuItem1.Text = "Women's"
+        '
+        'FullToolStripMenuItem
+        '
+        Me.FullToolStripMenuItem.Name = "FullToolStripMenuItem"
+        Me.FullToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.FullToolStripMenuItem.Text = "Full"
+        '
+        'HalfToolStripMenuItem
+        '
+        Me.HalfToolStripMenuItem.Name = "HalfToolStripMenuItem"
+        Me.HalfToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.HalfToolStripMenuItem.Text = "Half"
+        '
+        'FullToolStripMenuItem1
+        '
+        Me.FullToolStripMenuItem1.Name = "FullToolStripMenuItem1"
+        Me.FullToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.FullToolStripMenuItem1.Text = "Full"
+        '
+        'HalfToolStripMenuItem1
+        '
+        Me.HalfToolStripMenuItem1.Name = "HalfToolStripMenuItem1"
+        Me.HalfToolStripMenuItem1.Size = New System.Drawing.Size(152, 22)
+        Me.HalfToolStripMenuItem1.Text = "Half"
+        '
+        'GPSToolStripMenuItem
+        '
+        Me.GPSToolStripMenuItem.Name = "GPSToolStripMenuItem"
+        Me.GPSToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.GPSToolStripMenuItem.Text = "GPS"
+        '
+        'RadioToolStripMenuItem
+        '
+        Me.RadioToolStripMenuItem.Name = "RadioToolStripMenuItem"
+        Me.RadioToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.RadioToolStripMenuItem.Text = "Radio"
+        '
+        'LightingToolStripMenuItem
+        '
+        Me.LightingToolStripMenuItem.Name = "LightingToolStripMenuItem"
+        Me.LightingToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.LightingToolStripMenuItem.Text = "Lighting"
+        '
+        'SkullToolStripMenuItem
+        '
+        Me.SkullToolStripMenuItem.Name = "SkullToolStripMenuItem"
+        Me.SkullToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SkullToolStripMenuItem.Text = "Skull"
+        '
+        'CrossToolStripMenuItem
+        '
+        Me.CrossToolStripMenuItem.Name = "CrossToolStripMenuItem"
+        Me.CrossToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.CrossToolStripMenuItem.Text = "Cross"
+        '
+        'SymobolicToolStripMenuItem
+        '
+        Me.SymobolicToolStripMenuItem.Name = "SymobolicToolStripMenuItem"
+        Me.SymobolicToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SymobolicToolStripMenuItem.Text = "Symobolic"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(625, 500)
+        Me.Name = "Form1"
+        Me.Text = "Main"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents RidingGearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MensToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents WomensToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelmetsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ElectronicsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AccessoriesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents JacketsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LeatherToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PantsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LeatherToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents TextileToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents BootsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GlovesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LeatherToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents TextileToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents MensToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents FullToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HalfToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents WomensToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents FullToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents HalfToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents GPSToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RadioToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LightingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SkullToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CrossToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SymobolicToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ TEST/Test04272016/Test04272016/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub ContextMenuStrip1_Opening(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles ContextMenuStrip1.Opening
+
+    End Sub
+End Class
